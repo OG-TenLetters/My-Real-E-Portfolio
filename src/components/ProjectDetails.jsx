6c47e6eba0609848c8ffb749bf0d15842b24ce32
Jadon <1tenletters0@gmail.com>
2025-05-13 10:41:07 -0700

    Okay I got it working, but I can't seem to figure out why the index is being ignored for it's initial positioning..

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -5,56 +5,61 @@ import "aos/dist/aos.css";
 import { useEffect } from "react";
 
 const ProjectDetails = ({ closeProject }) => {
-    useEffect(() => {
-        Aos.init();
-    }, [closeProject])
+  useEffect(() => {
+    Aos.init();
+  }, [closeProject]);
   return (
     <div className="project__details--container project__details--container-active">
+      <div data-aos="fade-in" data-aos-delay="350" className="project__details">
+        <FontAwesomeIcon
+          onClick={() => closeProject()}
+          className="project__exit"
+          icon={faTimes}
+        />
         <div
-        data-aos="zoom-in-up"
-        className="project__details">
-          <FontAwesomeIcon
-            onClick={() => closeProject()}
-            className="project__exit"
-            icon={faTimes}
-          />
-          <div data-aos="fade-in" className="project__title">
-            Anithon
-          </div>
-          <p data-aos="fade-down" className="project__para">
-            This is a from scratch project that I used to practice building up my
-            styling skills and get familiar with useState, useEffect, useConext etc.
-            Along with API integration
-          </p>
-          <div className="project__links">
-            <a
-              target="_blank"
-              href="https://github.com/OG-TenLetters/Anime-List-React"
-            >
-              <div className="project__link">
-                <p>Github</p>
-              </div>
-            </a>
-            <a target="_blank" href="https://anime-list-react-orpin.vercel.app/">
-              <div className="project__link">
-                <p>
-                  Live <br />
-                  View
-                </p>
-              </div>
-            </a>
-          </div>
-          <div className="used-techs">
-            <p>Tech Used:</p>
-            <div className="used-tech">
-              React
-            </div>
-            <div
-            data-aos="fade-up-left"
-            className="used-tech">HTML</div>
-            <div className="used-tech">CSS</div>
-          </div>
+          data-aos="fade-in"
+          data-aos-delay="100"
+          data-aos-duration="100"
+          className="project__title"
+        >
+          Anithon
         </div>
+        <p
+          data-aos="fade-in"
+          data-aos-delay="100"
+          data-aos-duration="1000"
+          data-aos-easing="ease-in-sine"
+          className="project__para"
+        >
+          This is a from scratch project that I used to practice building up my
+          styling skills and get familiar with useState, useEffect, useConext
+          etc. Along with API integration
+        </p>
+        <div className="project__links">
+          <a
+            target="_blank"
+            href="https://github.com/OG-TenLetters/Anime-List-React"
+          >
+            <div className="project__link">
+              <p>Github</p>
+            </div>
+          </a>
+          <a target="_blank" href="https://anime-list-react-orpin.vercel.app/">
+            <div className="project__link">
+              <p>
+                Live <br />
+                View
+              </p>
+            </div>
+          </a>
+        </div>
+        <div className="used-techs">
+          <p>Tech Used:</p>
+          <div className="used-tech">React</div>
+          <div className="used-tech">HTML</div>
+          <div className="used-tech">CSS</div>
+        </div>
+      </div>
     </div>
   );
 };
